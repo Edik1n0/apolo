ce6a84097dd939fb2f898ba730f18d90dae94fbd
Farsali <35901018+Edik1n0@users.noreply.github.com>
2021-06-08 14:33:27 -0500

    first commit

---
+++ database/control.sql
@@ -0,0 +1,14 @@
+-- CREATE TABLE clients(
+-- 	id INT(11) NOT NULL,
+--     nombre VARCHAR(100) NOT NULL,
+--     telefono INT(60) NOT NULL,
+--     email VARCHAR(60) NOT NULL,
+--     descripcion text NOT NULL,
+--     producto VARCHAR(60) NOT NULL
+-- );
+
+-- ALTER TABLE clients
+-- 	MODIFY telefono VARCHAR(60) NOT NULL;
+
+-- ALTER TABLE cusers
+-- 	ADD PRIMARY KEY (id);
---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'b99dd32b9293bc',
+        password: 'c502fb27',
+        database: 'heroku_412f3b1d483a5b8'
+    }
+};
