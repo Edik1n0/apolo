46019ce0565af5be906dcd6d28e3449c3e6b105d
Farsali <35901018+Edik1n0@users.noreply.github.com>
2021-06-08 17:46:24 -0500

    quinto commit

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,17 @@
 module.exports = {
     database: {
         host: 'us-cdbr-east-03.cleardb.com',
-        user: 'be7135c7eb02c0',
-        password: '57475ea8',
-        database: 'heroku_cea3c0fd4f250a2'
+        user: 'b59504b7bc764c',
+        password: '6c001fa3',
+        database: 'heroku_5ef31bb57b72aaa'
     }
 };
+
+// module.exports = {
+//     database: {
+//         host: 'us-cdbr-east-03.cleardb.com',
+//         user: 'be7135c7eb02c0',
+//         password: '57475ea8',
+//         database: 'heroku_cea3c0fd4f250a2'
+//     }
+// };
