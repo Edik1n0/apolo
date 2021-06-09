1c110750fbb6fce1a662d150b2f07df38952c817
Farsali <35901018+Edik1n0@users.noreply.github.com>
2021-06-09 11:40:56 -0500

    subo seis

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,26 @@
+//  module.exports = {
+//      database: {
+//          host: 'us-cdbr-east-03.cleardb.com',
+//          user: 'b59504b7bc764c',
+//          password: '6c001fa3',
+//          database: 'heroku_5ef31bb57b72aaa'
+//      }
+//  };
+
 // module.exports = {
 //     database: {
 //         host: 'us-cdbr-east-03.cleardb.com',
-//         user: 'b59504b7bc764c',
-//         password: '6c001fa3',
-//         database: 'heroku_5ef31bb57b72aaa'
+//         user: 'be7135c7eb02c0',
+//         password: '57475ea8',
+//         database: 'heroku_cea3c0fd4f250a2'
 //     }
 // };
 
 module.exports = {
     database: {
-        host: 'us-cdbr-east-03.cleardb.com',
-        user: 'be7135c7eb02c0',
-        password: '57475ea8',
-        database: 'heroku_cea3c0fd4f250a2'
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database: 'demo'
     }
 };
