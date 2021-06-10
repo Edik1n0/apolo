ace43db849717260973981f9e0aa128d16543159
Farsali <35901018+Edik1n0@users.noreply.github.com>
2021-06-10 16:29:47 -0500

    db again

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,17 @@
+// module.exports = {
+//     database: {
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: '',
+//         database: 'demo'
+//     }
+// };
+
 module.exports = {
     database: {
-        host: '127.0.0.1',
-        user: 'root',
-       password: '',
-        database: 'demo'
+        host: '',
+        user: '',
+        password: '',
+        database: ''
     }
 };
