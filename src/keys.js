4daca6643b2b121eff5dfbc354c9fca04bfbf937
Farsali <35901018+Edik1n0@users.noreply.github.com>
2021-06-11 13:58:58 -0500

    ..

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-module.exports = {
-    database: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'demo'
-    }
-};
-
 // module.exports = {
 //     database: {
-//         host: 'us-cdbr-east-03.cleardb.com',
-//         user: 'b99dd32b9293bc',
-//         password: 'c502fb27',
-//         database: 'heroku_412f3b1d483a5b8'
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: '',
+//         database: 'demo'
 //     }
 // };
+
+module.exports = {
+    database: {
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'b99dd32b9293bc',
+        password: 'c502fb27',
+        database: 'heroku_412f3b1d483a5b8'
+    }
+};
